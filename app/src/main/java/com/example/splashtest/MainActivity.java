f3faeed59717633523263ad5b3b0f16ace489b43
YuJie <mmmmhhhhhhhhhhhh@gmail.com>
2019-11-21 20:34:03 +0800

    10/21

--- app/src/main/java/com/example/splashtest/MainActivity.java
+++ app/src/main/java/com/example/splashtest/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.splashtest;
-
+//1mipmap放張圖
+//intent進階版(系統自動切換activity)
+//2新增empty activity
+//3拉imageView放該圖,定位
+//希望先執行新增的activity再到main activity
+//manifest會兩個activity
+//intent filter是一開始執行的意思
+//所以剪下貼到新增的activity的intent filter裡
+//換intent filter是方法1, 方法2是到程式碼畫面換activity
+//這邊老師少錄約30分鐘,到新增activity
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
--- app/src/main/java/com/example/splashtest/splashActivity.java
+++ app/src/main/java/com/example/splashtest/splashActivity.java
@@ -14,17 +14,22 @@ private ConstraintLayout act_in;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         Animation fadeIn = AnimationUtils.loadAnimation(splashActivity.this, R.anim.fade_in);
-        act_in = findViewById(R.id.carT);
-act_in.setAnimation(fadeIn);
+                 //變數名稱
+        act_in = findViewById(R.id.carT);//ConstrainLayout
+        act_in.setAnimation(fadeIn);//把淡入效果套進去
 
-
+//4
         Thread timer = new Thread(){
+//5右鍵產生overrideMethods, 選javalangThread裡的run():void
             @Override
             public void run(){
                 super.run();
+//6左邊res新增Animation resource file(動畫意思)出anim資料夾
+//alpha值是透明度
                 try {
-                    sleep(3000);
+                    sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
